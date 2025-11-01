6f7b62fd17f99914845b4feecb200ac28dbc008b
Nefeli <t8240032@aueb.gr>
2025-11-01 12:47:48 +0200

    Διορθωσα το μηνυμα

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("AAAAaaAAaaAaAAAAA");
+=======
+        System.out.println("Hello");
+>>>>>>> 91bec6c (Διορθωσα το μηνυμα)
     }
 }
+
