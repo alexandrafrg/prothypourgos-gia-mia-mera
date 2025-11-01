ffc680ced08f5c99759d51bb8fc922f579749aab
Giorgos Konstantidellis <g.kwnstadi@gmail.com>
2025-11-01 14:05:21 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("haha");
-        System.out.println("την εχουμε πουτσισει");
-        System.out.println("καλα ρε μαλακες");
+        System.out.println("hahagammmmxxreeeeeeeeeeeemmmmmmmmmmmmmmmmmmwwwwwwwwww");
     }
 }
