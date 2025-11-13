700c64be3465336914c5f1e198f3e72824de99c7
konstantinapapad <t8240103@aueb.gr>
2025-11-13 16:03:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("pame ligooooooooooo");
         System.out.println("yooooooo");
+        System.out.println("hiii");
     }
 <<<<<<< HEAD
 }
