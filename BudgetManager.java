1547e0b3aad4412cbaa8cccecf98188f38a384ee
nefeligk06 <t8240032@aueb.gr>
2025-11-24 19:16:49 +0200

    Update BudgetManager.java

--- BudgetManager.java
+++ BudgetManager.java
@@ -18,52 +18,6 @@ public class BudgetManager {
         System.out.println("Ισοζύγιο: " + (budget.getRevenue() - budget.getExpenses())); //Υπολογίζει και δείχνει έσοδα-έξοδα (τελικό αποτέλεσμα)
         System.out.println();
     }
-<<<<<<< HEAD
-
-    // Λειτουργία αλλαγής ποσού
-    public void modifyBudget(Scanner input) {
-
-        System.out.println("\nΣε ποια κατηγορία θέλεις αλλαγή;");
-        System.out.println("1. Παιδεία");
-        System.out.println("2. Υγεία");
-        System.out.println("3. Άμυνα");
-        System.out.println("4. Μεταφορές");
-        System.out.print("Επιλογή: ");
-
-        int choice = input.nextInt();
-
-        System.out.print("Δώσε νέο ποσό: ");
-        double newAmount = input.nextDouble();
-
-        if (newAmount < 0) {
-            System.out.println("Το ποσό δεν μπορεί να είναι αρνητικό!");
-            return;
-        }
-
-        switch (choice) {
-            case 1:
-                changesLog += "Παιδεία: " + budget.education + " → " + newAmount + "\n";
-                budget.education = newAmount;
-                break;
-            case 2:
-                changesLog += "Υγεία: " + budget.health + " → " + newAmount + "\n";
-                budget.health = newAmount;
-                break;
-            case 3:
-                changesLog += "Άμυνα: " + budget.defense + " → " + newAmount + "\n";
-                budget.defense = newAmount;
-                break;
-            case 4:
-                changesLog += "Μεταφορές: " + budget.transport + " → " + newAmount + "\n";
-                budget.transport = newAmount;
-                break;
-            default:
-                System.out.println("Μη έγκυρη επιλογή!");
-                return;
-        }
-
-        System.out.println("Η αλλαγή καταχωρήθηκε!\n");
-    }
 
     //εισαγωγη αλαγης ΝΕΦΕΛΗ
 =======
