8cae36ab408937d5d2df369a340f69351bec2e43
Giorgos Konstantidellis <g.kwnstadi@gmail.com>
2025-12-03 16:50:49 +0200

    Update BudgetManager.java

--- BudgetManager.java
+++ BudgetManager.java
@@ -10,5 +10,19 @@ public class BudgetManager {
         this.budget = budget;
     }
 
-    
+    // 1. ΠΡΟΒΟΛΗ ΠΡΟΥΠΟΛΟΓΙΣΜΟΥ
+    public void displayBudget() {
+        System.out.println("\n===== ΠΡΟΒΟΛΗ ΠΡΟΥΠΟΛΟΓΙΣΜΟΥ =====");
+        System.out.println("Συνολικά Έσοδα: " + budget.getRevenue());
+        System.out.println("Συνολικά Έξοδα: " + budget.getExpenses());
+        System.out.println("Ισοζύγιο: " + (budget.getRevenue() - budget.getExpenses()));
+
+        System.out.println("\nΑνάλυση ανά Υπουργείο:");
+        for (int i = 0; i < budget.ministries.length; i++) {
+            System.out.println((i + 1) + ". " + budget.ministries[i] +
+                    "\n   Έσοδα: " + budget.ministryRevenue[i] +
+                    "\n   Έξοδα: " + budget.ministryExpenses[i] + "\n");
+        }
+    } 
 }
+ 
