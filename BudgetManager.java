13d9b667d2700a28595617311676606e574436d4
konstantinapapad <t8240103@aueb.gr>
2025-12-03 16:47:24 +0200

    Update BudgetManager.java

--- BudgetManager.java
+++ BudgetManager.java
@@ -1,2 +1,14 @@
- 
- 
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BudgetManager {
+
+    private Budget budget;
+    private StringBuilder changesLog = new StringBuilder();
+
+    public BudgetManager(Budget budget) {
+        this.budget = budget;
+    }
+
+    
+}
