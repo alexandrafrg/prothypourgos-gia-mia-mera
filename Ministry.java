3a9d4921d495c2e83ac21061a4fdc07226ce6045
myrtobalasi <t8240273@aueb.gr>
2025-12-22 23:15:16 +0200

    Refactor Budget: add Ministry class and recalculate totals

---
+++ Ministry.java
@@ -0,0 +1,36 @@
+public class Ministry {
+
+    private String name;
+    private double revenue;
+    private double expenses;
+
+    public Ministry(String name, double revenue, double expenses) {
+        this.name = name;
+        this.revenue = revenue;
+        this.expenses = expenses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public double getExpenses() {
+        return expenses;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
+
+    public void setExpenses(double expenses) {
+        this.expenses = expenses;
+    }
+
+    public double getBalance() {
+        return revenue - expenses;
+    }
+}
